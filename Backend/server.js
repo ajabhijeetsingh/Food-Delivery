e805423daede64bd3ec9574645a1b6f509a6598b
ajabhijeetsingh <abhijeetsingh250.as@gmail.com>
2024-06-26 21:33:38 +0530

    main

---
+++ Backend/server.js
@@ -0,0 +1,30 @@
+import express from "express"
+import cors from "cors"
+import { connect } from "mongoose"
+import connectDB from "./config/db.js"
+
+
+
+
+// app config
+const app = express()
+const port = 4000
+
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+//db connection
+
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("API is Working")
+})
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`)
+})
+
+//mongodb+srv://aj_abhijeetsingh:pulsar150@cluster0.1d8wqln.mongodb.net/?
