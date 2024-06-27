29e37f650cc476036e4469ca602212897a77fe2d
ajabhijeetsingh <abhijeetsingh250.as@gmail.com>
2024-06-27 16:26:37 +0530

    main

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { connect } from "mongoose"
 import connectDB from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
 
 
 
@@ -18,6 +19,13 @@ app.use(cors())
 //db connection
 
 connectDB();
+
+// api endpoints 
+
+app.use("/api/food",foodRouter)
+app.use("/images",express.static('uploads'))
+
+
 
 app.get("/", (req, res) => {
     res.send("API is Working")
