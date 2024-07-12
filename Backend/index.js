b27e1387a96a570f5afae3a4486f78f7274afa7a
ajabhijeetsingh <abhijeetsingh250.as@gmail.com>
2024-07-12 18:13:53 +0530

    main

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,16 @@ const port = 4000
 
 //middleware
 app.use(express.json())
-app.use(cors())
+
+app.use(cors(
+    {
+        origin: [""],
+        methods: ["POST","GET"],
+        credentials: true
+    }
+))
+
+
 
 //db connection
 
