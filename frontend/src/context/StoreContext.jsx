086d707aa8a75b43654a699a36c0ceb00a928e1f
ajabhijeetsingh <abhijeetsingh250.as@gmail.com>
2024-07-12 19:00:39 +0530

    main

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "https://food-delivery-back-lilac.vercel.app/";
+    const url = "https://food-delivery-back-lilac.vercel.app";
     const [token, setToken] = useState("");
     const [food_list, setFoodlist] = useState([])
 
