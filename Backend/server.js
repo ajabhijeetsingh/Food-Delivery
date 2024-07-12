84854a295fdeca61b11086e3ce65ac9fb75783ac
ajabhijeetsingh <abhijeetsingh250.as@gmail.com>
2024-07-12 17:05:03 +0530

    main

--- Backend/server.js
+++ Backend/server.js
@@ -3,10 +3,10 @@ import cors from "cors"
 import { connect } from "mongoose"
 import connectDB from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
-// import userRouter from "./routes/userRoute.js"
 import userRouter from "./routes/userRoute.js"
 import 'dotenv/config'
 import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
 
 
@@ -30,6 +30,7 @@ app.use("/api/food", foodRouter)
 app.use("/images", express.static('uploads'))
 app.use("/api/user", userRouter)
 app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter);
 
 
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,20 +6,24 @@ import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
+import Verify from './pages/Verify/verify'
+
 
 const App = () => {
 
-  const [showLogin,setShowLogin]=useState(false);
+  const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
-      {showLogin?<LoginPopup setShowLogin={setShowLogin}></LoginPopup>:<></>}
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin}></LoginPopup> : <></>}
       <div className='app'>
-        <Navbar setShowLogin={setShowLogin}/>
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/order' element={<PlaceOrder />} />
+          <Route path='/verify' element={<Verify />} />
+
         </Routes>
       </div>
       <Footer />
