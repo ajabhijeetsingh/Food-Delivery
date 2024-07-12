d92ba56c1e849178fc8623cd5f4caf48c4abf7b4
ajabhijeetsingh <abhijeetsingh250.as@gmail.com>
2024-07-12 18:50:21 +0530

    main

--- Backend/routes/foodRoute.js
+++ Backend/routes/foodRoute.js
@@ -8,14 +8,14 @@ const foodRouter = express.Router();
 // Image Storage Engine
 
 const storage = multer.diskStorage({
-    // destination: "uploads",
-    destination: function (req, file, cb) {
-        cb(null, './uploads'); // Assuming 'uploads' directory exists and is writable
-    },
+    destination: "uploads",
+    // destination: function (req, file, cb) {
+    //     cb(null, './uploads'); // Assuming 'uploads' directory exists and is writable
+    // },
     filename:(req,file,cb) =>{
-        // return cb(null,`${Date.now()}${file.originalname}`)
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.mimetype.split('/')[1]);
+        return cb(null,`${Date.now()}${file.originalname}`)
+        // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        // cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.mimetype.split('/')[1]);
     }
 })
 
