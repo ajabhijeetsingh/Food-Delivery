1cf0fec5632a268b9b9dc50ea36fdb34ef45ea74
Abhijeet Singh <102654858+ajabhijeetsingh@users.noreply.github.com>
2024-07-25 15:00:44 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json())
 
 app.use(cors(
     {
-        origin: ["https://abhijeet-ka-dhaba.vercel.app","https://food-delivery-rho-nine.vercel.app"],
+        origin: ["http://localhost:5176","https://abhijeet-ka-dhaba.vercel.app","https://food-delivery-rho-nine.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
