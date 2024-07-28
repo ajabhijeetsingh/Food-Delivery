c02fe770bdc611c5c6f8532c93523b1f00e1c24a
Abhijeet Singh <102654858+ajabhijeetsingh@users.noreply.github.com>
2024-07-28 17:27:07 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -45,6 +45,7 @@ const StoreContextProvider = (props) => {
     const fetchFoodList = async () => {
         // const response = await axios.get(url + "/api/food/list");
         const response = await axios.get(`${url}/api/food/list`);
+        console.log(response);
         setFoodlist(response.data.data);
     }
 
